037b14910db7576f43bf0f64b3937a50d3593ebf
chrsolr <chr.solr@gmail.com>
2023-10-06 16:39:15 -0500

    Added required to about DTO

--- DTOs/AboutDTO.cs
+++ DTOs/AboutDTO.cs
@@ -2,10 +2,10 @@ namespace chrsolr_api.DTOs;
 
 public class AboutDTO
 {
-    public string FirstName { get; set; }
-    public string LastName { get; set; }
-    public string NickName { get; set; }
-    public string[] AboutMe { get; set; }
-    public string ImageUrl { get; set; }
+    public required string FirstName { get; set; }
+    public required string LastName { get; set; }
+    public required string NickName { get; set; }
+    public required string[] AboutMe { get; set; }
+    public required string ImageUrl { get; set; }
     public required List<SocialDTO> Socials { get; set; }
 }
