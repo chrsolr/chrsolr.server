fe7b39d02b6655d8a0de81e7009f0ae72eac9dcf
chrsolr <chr.solr@gmail.com>
2023-10-05 22:40:31 -0500

    WIP: GET About Endpoint

---
+++ Controllers/AboutController.cs
@@ -0,0 +1,28 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace chrsolr_api.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class AboutController : ControllerBase
+{
+    private readonly ILogger<AboutController> _logger;
+
+    public AboutController(ILogger<AboutController> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpGet(Name = "GetAbout")]
+    public About Get()
+    {
+        var about = new About
+        {
+            FirstName = "Christian",
+            LastName = "Soler",
+            NickName = "Chrsolr"
+        };
+
+        return about;
+    }
+}
---
+++ Models/About.cs
@@ -0,0 +1,8 @@
+namespace chrsolr_api;
+
+public class About
+{
+    public required string FirstName { get; set; }
+    public required string LastName { get; set; }
+    public required string NickName { get; set; }
+}
