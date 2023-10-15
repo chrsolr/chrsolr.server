8f5cf187c9a72ba84faf5db1ec095a6d89cf4251
iwchrsolr <christian@innowatts.com>
2023-10-14 20:10:09 -0500

    Updated

--- Models/User.cs
+++ Models/User.cs
@@ -8,11 +8,11 @@ public class User
     public required string Username { get; set; }
 
     [Required]
-    [MinLength(8)] 
+    [MinLength(8)]
     [MaxLength(128)]
     public required string Password { get; set; }
     public required string FullName { get; set; }
-    public required DateTime CreatedAt { get; set; }
+    public required DateTime CreatedAt { get; set; } = DateTime.UtcNow;
     public required DateTime UpdatedAt { get; set; }
     public required DateTime LastLoggedInAt { get; set; }
 }
